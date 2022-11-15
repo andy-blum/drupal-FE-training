ab8ec4ef4b0865fe1d26e09565e6cb92c3283c56
Andy Blum <andy.blum.01@gmail.com>
2022-11-15 05:19:47 +0000

    use once() to avoid duplicating work

--- web/themes/custom/foomami/js/color-picker.js
+++ web/themes/custom/foomami/js/color-picker.js
@@ -1,7 +1,10 @@
 ((Drupal) => {
   Drupal.behaviors.foomami_color = {
     attach: () => {
-      
+      const textInputs = once(
+        'foomami-color',
+        '[data-drupal-selector="foomami-color-picker"] > input[type="text"]',
+      );
     },
   };
 })(Drupal)
