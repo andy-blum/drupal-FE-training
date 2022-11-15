1b094fafce8abed3671d3b2bdca326e23cef5b5d
Andy Blum <andy.blum.01@gmail.com>
2022-11-15 05:05:09 +0000

    change primary_color to text input

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -11,9 +11,18 @@ function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $fo
     '#title' => t('Colors'),
     '#open' => true,
     'primary_color' => [
-      '#type' => 'color',
+      '#type' => 'textfield',
+      '#maxlength' => 7,
+      '#size' => 10,
       '#title' => t('Primary Color'),
+      '#description' => t('Enter color in full hexadecimal format (#abc123). <br/> Derivatives will be formed from this color.'),
       '#default_value' => theme_get_setting('primary_color'),
+      '#attributes' => [
+        'pattern' => '^#[a-fA-F0-9]{6}',
+      ],
+      '#wrapper_attributes' => [
+        'data-drupal-selector' => 'foomami-color-picker'
+      ]
     ]
   ];
 }
