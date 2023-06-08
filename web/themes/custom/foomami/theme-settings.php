7eba7f887a939fa877877cc185c52fa8122bcc53
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 20:33:42 -0400

    Add brand color field

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -10,5 +10,10 @@ function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $fo
     '#type' => 'details',
     '#title' => t('Colors'),
     '#open' => true,
+    'brand_color' => [
+      '#type' => 'color',
+      '#title' => t('Brand Color'),
+      '#default_value' => theme_get_setting('brand_color'),
+    ],
   ];
 }
