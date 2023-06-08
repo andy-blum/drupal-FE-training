2419c45eba64ec3ab97566faa27b109f24d9b045
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 20:23:59 -0400

    fix theme-settings hook

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -3,6 +3,6 @@
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function theme-settings_form_FORM_ID_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+function foomami_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
 
 }
