c1250fcb767c88f173b6877a7bd60315513f51d3
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 20:29:25 -0400

    Add 'colors' fieldgroup

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -6,5 +6,9 @@ use \Drupal\Core\Form\FormStateInterface;
  * Implements hook_form_FORM_ID_alter().
  */
 function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
-
+  $form['colors'] = [
+    '#type' => 'details',
+    '#title' => t('Colors'),
+    '#open' => true,
+  ];
 }
