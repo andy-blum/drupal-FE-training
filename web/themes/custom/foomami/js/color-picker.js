59cd99fdb15ab06268957a647bba9898b1e94604
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 20:57:19 -0400

    add once() to behavior

--- web/themes/custom/foomami/js/color-picker.js
+++ web/themes/custom/foomami/js/color-picker.js
@@ -1,9 +1,13 @@
-((Drupal) => {
+((Drupal, once) => {
   Drupal.behaviors.foomami_color = {
     attach: (context, settings) => {
-      console.log('ATTACHED');
-      console.log(context);
-      console.log(settings);
+      const colorFields = once(
+        'foomami-color-picker',
+        '[data-drupal-selector="foomami-color-picker"]'
+      );
+
+      console.log('ATTACHED!');
+      console.log(colorFields);
     },
   }
-})(window.Drupal);
+})(window.Drupal, window.once);
