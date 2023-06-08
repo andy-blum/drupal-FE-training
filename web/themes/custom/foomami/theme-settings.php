16127f3fc8760766535206ffff2e122a578b972c
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 20:26:55 -0400

    simplify theme-settings hook

--- web/themes/custom/foomami/theme-settings.php
+++ web/themes/custom/foomami/theme-settings.php
@@ -1,8 +1,10 @@
 <?php
 
+use \Drupal\Core\Form\FormStateInterface;
+
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function foomami_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+function foomami_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
 
 }
