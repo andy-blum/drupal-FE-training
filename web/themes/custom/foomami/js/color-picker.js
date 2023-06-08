4cc15c171d16d74b13e0d803e937113f717f11b6
Andy Blum <andy.blum.01@gmail.com>
2023-06-07 21:12:02 -0400

    Add color picker functionality

--- web/themes/custom/foomami/js/color-picker.js
+++ web/themes/custom/foomami/js/color-picker.js
@@ -6,8 +6,35 @@
         '[data-drupal-selector="foomami-color-picker"]'
       );
 
-      console.log('ATTACHED!');
-      console.log(colorFields);
+      colorFields.forEach(field => {
+        // get the <input type="text"> element
+        const textInput = field.querySelector('input[type="text"]');
+
+        // create a new <input type="color"> element
+        const colorInput = document.createElement('input');
+        colorInput.type = 'color';
+
+        // Add Drupal's form-element classes
+        colorInput.classList.add(
+          'form-element',
+          'form-element--type-color',
+        );
+
+        // Set default value
+        colorInput.value = textInput.value;
+
+        // Add element to DOM
+        textInput.after(colorInput);
+
+        // Watch each input for changes, keep inputs synced
+        textInput.addEventListener('input', () => {
+          colorInput.value = textInput.value;
+        });
+
+        colorInput.addEventListener('input', () => {
+          textInput.value = colorInput.value;
+        });
+      });
     },
   }
 })(window.Drupal, window.once);
